3459aab962131a1bf812693dde60eb851b84e428
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-06-23 21:33:13 +0200

    Add initial Terraform configuration for security groups and rules

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "stackit_security_group" "example" {
+  project_id = var.project_id
+  name       = var.name
+  labels = {
+    "description" = var.rules["http-80-tcp"].value[4]
+  }
+}
+
+resource "stackit_security_group_rule" "ingress" {
+  project_id        = var.project_id
+  security_group_id = stackit_security_group.example.id
+  direction         = "ingress"
+  ether_type        = "IPv4"
+
+
+  dynamic "port_range" {
+    for_each = var.rules["http-80-tcp"]
+    content {
+      from = setting.value[0]
+      to   = setting.value[1]
+    }
+  }
+
+  dynamic "protocol" {
+    for_each = var.rules["http-80-tcp"]
+    content {
+      name = setting.value[3]
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "rules" {
+  type = map(list(any))
+  default = {
+    "http-80-tcp"    = [80, 80, "tcp", "HTTP"]
+    "http-8080-tcp"  = [8080, 8080, "tcp", "HTTP"]
+    "https-443-tcp"  = [443, 443, "tcp", "HTTPS"]
+    "https-8443-tcp" = [8443, 8443, "tcp", "HTTPS"]
+    "ssh-22-tcp"     = [22, 22, "tcp", "SSH"]
+  }
+}
