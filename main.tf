422891d3ffd1e3b5518de9c641ad05dde5d4db1d
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-06-25 19:26:31 +0200

    Refactor security group resource to use variable rules for name and protocol

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 resource "stackit_security_group" "example" {
   project_id = var.project_id
-  name       = "http-80-tcp"
-  labels = {
-    "description" = var.rules["http-80-tcp"][4]
-  }
+  name       = var.rules["http-80-tcp"][3]
 }
 
 resource "stackit_security_group_rule" "ingress" {
@@ -12,19 +9,12 @@ resource "stackit_security_group_rule" "ingress" {
   direction         = "ingress"
   ether_type        = "IPv4"
 
-
-  dynamic "port_range" {
-    for_each = var.rules["http-80-tcp"]
-    content {
-      from = setting.value[0]
-      to   = setting.value[1]
-    }
+  port_range {
+    from = var.rules["http-80-tcp"][0]
+    to   = var.rules["http-80-tcp"][1]
   }
 
-  dynamic "protocol" {
-    for_each = var.rules["http-80-tcp"]
-    content {
-      name = setting.value[3]
-    }
+  protocol {
+    name = var.rules["http-80-tcp"][3]
   }
 }
