32a5324bcf21296c7266c7472f16db6e0dd282fa
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-06-26 18:04:35 +0200

    Add duplicate ingress rule for IPv6 in security group configuration

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource "stackit_security_group_rule" "ingress" {
     name = var.rules[var.rule_name][2]
   }
 }
+
+resource "stackit_security_group_rule" "ingress" {
+  project_id        = var.project_id
+  security_group_id = stackit_security_group.example.security_group_id
+  direction         = "ingress"
+  ether_type        = "IPv6"
+
+  port_range = {
+    min = var.rules[var.rule_name][0]
+    max = var.rules[var.rule_name][1]
+  }
+
+  protocol = {
+    name = var.rules[var.rule_name][2]
+  }
+}
